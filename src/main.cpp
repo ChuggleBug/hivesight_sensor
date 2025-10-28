27b0e73aaf8512d70f49414add9e7f2747bec0b9
Jacob <jacobgutierrez752@gmail.com>
2025-10-27 21:47:36 -0700

    Update build script

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+WiFiClient netif;
+PubSubClient mqttClient(netif);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.begin(BAUD_RATE);
+
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
