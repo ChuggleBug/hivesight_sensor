daed341878c5cef7402d315fb99f5ee33550a92b
Jacob <jacobgutierrez752@gmail.com>
2025-11-26 08:33:06 -0800

    Add template file for config json

---
+++ config.json.d
@@ -0,0 +1,13 @@
+{
+    "WiFiSSID": "myssid",
+    "WiFiPassword": "mypass",
+    "DeviceName": "DevName",
+    "MQTT": {
+        "IP": "192.168.0.1",
+        "Port": 1883
+    },
+    "HTTP": {
+        "IP": "192.168.0.1",
+        "Port": 80
+    }
+}
